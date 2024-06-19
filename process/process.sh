3767c0129f928d4e628922f0cad93739917a9436
DuarteDc <al221910409@gmail.com>
2024-06-18 22:46:26 -0600

    Add: Manage process

---
+++ process/process.sh
@@ -0,0 +1,14 @@
+#Muestra los procesos asociados a una terminal
+ps
+#Muestra todos los procesos
+ps x
+
+#Muestra información mas detallada de los procesos
+ps aux | less
+
+#Top muestra los procesos en ejecución de forma dinamica
+top
+# S Para cambiar el intervalo de actualización
+# U Para filtrar por usuario
+# E Para cambiar el valor de consumo del proceso
+# O Filtrar por columna de top
